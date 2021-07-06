f978e0ddd510abee2db8c46e50a77ac504985999
CedricNozerand <cedric.nozerand@gmail.com>
2021-07-06 16:06:10 +0200

    Insertion du front de la commande

--- src/main/java/com/ensup/myresto/controller/CommandeController.java
+++ src/main/java/com/ensup/myresto/controller/CommandeController.java
@@ -1,19 +1,12 @@
 package com.ensup.myresto.controller;
 
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ensup.myresto.domaine.Command;
 import com.ensup.myresto.domaine.CommandStatus;
